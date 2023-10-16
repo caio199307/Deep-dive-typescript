985c03924fcfd6539d8cf959cc31d417a76229e8
Caio Vitor <caio199307@gmail.com>
2023-10-16 14:38:31 -0300

    Add first page and initial configurations

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+function component() {
+    const element = document.getElementById('content');
+
+    element.innerHTML = "<h1>Olá moundo</h1>"
+    element.innerHTML += "<h1>Olá moundo</h1>"
+    element.innerHTML += "<h1>Olá moundo</h1>"
+
+
+    return element;
+  }
+
+ component();
