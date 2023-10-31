2b0dadbec9884c54f44c526d475e156539c6c0a6
Caio Vitor <caio199307@gmail.com>
2023-10-31 09:52:50 -0300

    added generics to the project

--- src/output.ts
+++ src/output.ts
@@ -1,4 +1,5 @@
 import { accessOptions, User } from "./models";
+import { IService, UserService } from "./service/service";
 
 class UserController {
   //propriedades
@@ -23,7 +24,8 @@ class UserController {
 
   //funcionalidades
   async userLayout(): Promise<void> {
-    const users: User[] = await this.getUser();
+    const service: IService<User> = new UserService<User>();
+    const users = await service.getItems();
 
     users.map((user: User) => {
       this.content.innerHTML += <string>this.createLine(user);
@@ -40,12 +42,6 @@ class UserController {
       `;
     });
     (this.getFormElement('#accessRadio0')).checked = true;
-  };
-
-  async getUser(): Promise<User[]> {
-    const response: Response = await fetch('http://localhost:5011/users');
-    const users: User[] = await response.json();
-    return users;
   };
 
   addEmployee (): void {
